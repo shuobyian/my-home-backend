8d72a4eb892c9ef3c1f4b879634ba95257e5bca0
shuobyian <chickby@gmail.com>
2023-08-08 22:18:30 +0900

    설정 파일

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Item } from 'src/item/entities/item.entity';
+import { ItemModule } from 'src/item/item.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: Number(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.postgres,
+      entities: [Item],
+      synchronize: true,
+    }),
+    ItemModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
